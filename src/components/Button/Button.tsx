b060ebafd0a5250a98bb43faa592004eab369cee
noxtros <mr.kevinkalus@gmail.com>
2025-07-04 19:03:38 +0200

    added className for button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,13 +3,14 @@ import React from "react";
 interface ButtonProps {
   label: string;
   action: () => void;
+  className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ label, action }) => {
+export const Button: React.FC<ButtonProps> = ({ label, action, className }) => {
   return (
     <button
       onClick={action}
-      className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+      className={`rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 ${className}`}
     >
       {label}
     </button>
