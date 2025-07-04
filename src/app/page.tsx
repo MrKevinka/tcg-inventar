6797c11e5cb1d4124dee96c05295086feaf99139
noxtros <mr.kevinkalus@gmail.com>
2025-07-04 18:17:29 +0200

    renamed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Collection } from "@/components/Collection/collection";
+import { CardList } from "@/components/CardList/CardList";
 
 export default function Home() {
   return (
-    <main className="mx-5">
-      <Collection />
+    <main className="mx-5 flex flex-col">
+      <CardList />
     </main>
   );
 }
