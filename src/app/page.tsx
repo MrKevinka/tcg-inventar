cfc26aeb1120b0700e275ad565568af3ac569102
noxtros <mr.kevinkalus@gmail.com>
2025-08-13 11:32:38 +0200

    removed images and changed margin on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { CardList } from '@/components/CardList/CardList';
 
 export default function Home() {
   return (
-    <main className="mx-5 flex flex-col" id="start">
+    <main className="m-5 flex flex-col" id="start">
       <CardList />
     </main>
   );
